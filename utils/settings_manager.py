51fd9f5645cbbbeffdf0171f97f020972fdf61f6
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2025-02-14 02:03:22 -0300

    feat: Ensuring the generation of the json file in the executable application

--- utils/settings_manager.py
+++ utils/settings_manager.py
@@ -1,10 +1,15 @@
 import json
 import os
+import sys
+
+if getattr(sys, 'frozen', False):
+    BASE_DIR = sys._MEIPASS
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CONFIG_FILE = os.path.join(BASE_DIR, "config.json")
 
 DEFAULT_DOWNLOAD_PATH = os.path.join(os.path.expanduser("~"), "Downloads")
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CONFIG_FILE = os.path.join(BASE_DIR, "config.json")
 
 DEFAULT_SETTINGS = {
     "theme": "light",
